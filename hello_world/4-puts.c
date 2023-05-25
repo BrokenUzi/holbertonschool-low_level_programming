e319a6b0eee7142165439a88f17ca868a66b063f
root <jcup143@gmail.com>
2023-05-25 13:09:16 -0700

    $commitMessage

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
+/**
+ * function main is the starting point of the execution
+ * return makes the ending value 0
+ */
 
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle\n");
 		return (0);
 }
