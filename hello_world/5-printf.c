df476c8c7fad00f1264f898c997d5b6c67599cfd
root <jcup143@gmail.com>
2023-05-25 13:21:53 -0700

    $commitMessage

---
+++ hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - entry point of the program.
+ *
+ * Printf - create and output strings formatted at runtime.
+ *
+ * Return: 0
+ *
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
