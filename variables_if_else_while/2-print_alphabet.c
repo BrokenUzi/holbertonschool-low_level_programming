58cfd849eb7900ac0a1a0db960ba5e410d9bda6a
root <jcup143@gmail.com>
2023-05-26 14:52:21 -0700

    $commitMessage

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
-* main - function is the putchar which is used to write a chracter to the standard
+* main - putchar which is used to write a chracter to the standard
 * betty style doc for function main goes there
 * Return: always 0
 * a-z is the char for the entire alphabet in lowecase
@@ -10,10 +10,10 @@ int main(void)
 {
 	char letter = 'a';
 
-	while ( letter <= 'z' )
+	while (letter <= 'z')
 	{
 		putchar(letter);
-		( letter++ )
+		(letter++)
 	}
 
 	putchat('\n');
