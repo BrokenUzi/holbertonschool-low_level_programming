b4fe18c848465a5d95216dd3afbda75e91fbd182
root <jcup143@gmail.com>
2023-05-26 14:00:24 -0700

    $commitMessage

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -17,7 +17,7 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	LD = n % 10;
 	if (n > 5)
-		printf("Last digit of %d is %d and is 0\n", n, LD);
+		printf("Last digit of %d is %d and is greater than 5\n", n, LD);
 	if (n == 0)
 		printf("Last digit of %d is %d and is 0\n", n, LD);
 	if (n < 6 && n != 0)
