0fb59ebb2593eb42dd044b3fbe7e6f1a7be6daad
root <jcup143@gmail.com>
2023-05-26 10:43:49 -0700

    $commitMessage

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,2 +1,25 @@
-#include <stdio.h>
-main()
+#include <stdlib.h>
+#include <time.h>
+/**
+* main - function is the srand which generates a random number
+* betty style doc for function main goes there
+* Return: always 0
+*/
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n < 0)
+		printf("%d: is positive");
+	else
+		(n = 0)
+		printf("%d: is zero");
+	else
+		(n > 0)
+		printf("%d: is negative");
+	printf("n is: %d\n", a);
+	return (0);
+}
