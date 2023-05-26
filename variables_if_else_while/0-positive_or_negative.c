a0be6b6559aaad0a27807b6d7a107741fc989b07
root <jcup143@gmail.com>
2023-05-26 11:03:25 -0700

    $commitMessage

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -13,11 +13,10 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n < 0)
-		printf("%d: is positive");
-	else
-		(n = 0);
-		printf("%d: is zero");
+		printf("%d: is positive", n);
+	if (n = 0)
+		printf("%d: is zero", n);
 	if (n > 0)
-		printf("%d: is negative");
+		printf("%d: is negative", n);
 	return (0);
 }
