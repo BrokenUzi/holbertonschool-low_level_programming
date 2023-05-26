2d5c6ab7036451a3ddd2e199448d04f78a2858b3
root <jcup143@gmail.com>
2023-05-26 13:56:00 -0700

    $commitMessage

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -5,22 +5,23 @@
 * main - function is the srand which generates a random number
 * betty style doc for function main goes there
 * Return: always 0
+* LD stands for Last Digit
 */
 
 int main(void)
 {
 	int n;
-	int lastDigit;
+	int LD;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	lastDigit = n % 10;
+	LD = n % 10;
 	if (n > 5)
-		printf("Last digit of %d is %d and is greater than 5\n", n, lastDigit);
+		printf("Last digit of %d is %d and is greater than 5\n", n, LD);
 	if (n == 0)
-		printf("Last digit of %d is %d and is 0\n", n, lastDigit);
+		printf("Last digit of %d is %d and is 0\n", n, LD);
 	if (n < 6 && n != 0)
-		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastDigit);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, LD);
 
 	return (0);
 }
