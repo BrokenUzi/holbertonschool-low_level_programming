f5398ae2e75fcda98c60da64aaa242679db13431
root <jcup143@gmail.com>
2023-05-26 14:41:30 -0700

    $commitMessage

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+* main - function is the putchar which is used to write a chracter to the standard
+* betty style doc for function main goes there
+* Return: always 0
+* a-z is the char for the entire alphabet in lowecase
+*/
+
+int main(void)
+{
+	char ch = 'a-z';
+	putchar(ch);
+
+	a-z = ;
+	if ()
+	{
+		putchar("Last digit of %d is %d and is greater than 5\n", n, LD);
+	}
+	return (0);
+
+}
