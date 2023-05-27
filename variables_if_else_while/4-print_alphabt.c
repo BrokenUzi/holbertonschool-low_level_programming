5a91d8af0dd5d47de84aca64b3f2f256b9b04efa
root <jcup143@gmail.com>
2023-05-26 17:36:44 -0700

    $commitMessage

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+/**
+* main - putchar which is used to write a chracter to the standard
+* betty style doc for function main goes there
+* Return: always 0
+* a-z is the char for the entire alphabet in lowecase
+*/
+
+int main(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z'){
+	if (letter != 'e'){
+	
+	
+		putchar(letter);
+	}	
+		(letter++);
+	}
+
+	while (letter = 'f'){
+	if (letter != 'q'){
+	
+		putchar(letter);
+	}
+		(letter++);
+	}
+
+	putchar('\n');
+
+	return (0);
+
+}
