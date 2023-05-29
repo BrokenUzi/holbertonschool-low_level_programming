acf7b466a675118b1b3e03951aa151f95f9dce13
root <jcup143@gmail.com>
2023-05-28 19:01:50 -0700

    $commitMessage

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 /**
-*
-*
-*
+* main - putchar which is used to write a chracter to the standard
+* betty style doc for function main goes there
+* Return: always 0
+*  prints all possible combinations of single-digit numbers.
 *
 */
 int main(void)
