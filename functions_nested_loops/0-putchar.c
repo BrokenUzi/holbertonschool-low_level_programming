eb651ef07f6b14e7e98a12476f7f78dffb5f1c49
root <jcup143@gmail.com>
2023-05-29 15:16:21 -0700

    $commitMessage

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+*
+*
+*
+*
+*/
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('r');
+	putchar('\n');
+	return (0);
+}
---
+++ functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+Welcome to Functions, Nested loops!
