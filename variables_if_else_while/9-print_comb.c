2e91d95a102eb6109de8438b1fe772849c033569
root <jcup143@gmail.com>
2023-05-28 18:59:09 -0700

    $commitMessage

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+*
+*
+*
+*
+*/
+int main(void)
+{
+	int number = 0;
+	while (number < 10)
+	{
+	putchar('0' + number);
+	if (number < 9)
+		{	
+		putchar(',');
+		putchar(' ');
+		}
+	number++;
+	}
+	putchar('\n');
+	return (0);
+}
