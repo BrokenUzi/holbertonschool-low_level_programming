fcf9297288703833406e5977b1c484d5ebeb45a1
root <jcup143@gmail.com>
2023-05-29 18:22:31 -0700

    $commitMessage

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+* main - putchar which is used to write a chracter to the standard
+* betty style doc for function main goes there
+* Return: always 0
+* a-z is the char for the entire alphabet in lowecase
+*
+*/
+int main(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+		putchar(letter);
+		(letter++);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
