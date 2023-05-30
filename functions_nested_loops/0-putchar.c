730cfdba9791d6d44d06aef2aa2a03a45f5a628d
root <jcup143@gmail.com>
2023-05-29 18:17:42 -0700

    $commitMessage

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 /**
-*
-*
-*
+* main - putchar which is used to write a chracter to the standard
+* betty style doc for function main goes there
+* Return: always 0
+* This command prints "_putchar"
 *
 */
 int main(void)
