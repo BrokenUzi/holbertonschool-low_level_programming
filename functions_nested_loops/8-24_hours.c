6c485f4d62c00f4c0b3620577886a370882cc47a
root <jcup143@gmail.com>
2023-05-30 20:43:39 -0700

    $commitMessage

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -1,2 +1,20 @@
 #include <stdio.h>
+/**
+* jack_bauer - Bauer 24 hours
+*
+* Return: void
+*
+*/
+void jack_bauer(void)
+{
 
+int hour, minute;
+
+	for (hour = 0; hour < 24; hour++)
+	{
+	for (minute = 0; minute < 60; minute++)
+		{
+		printf("%02d:%02d\n", hour, minute);
+		}
+	}
+}
