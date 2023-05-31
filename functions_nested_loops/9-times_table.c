cc30f69f1eedcd5dcb7c9f407c5123d3a358cb4a
root <jcup143@gmail.com>
2023-05-30 20:48:32 -0700

    $commitMessage

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -1,2 +1,20 @@
 #include <stdio.h>
+/**
+* times_table - multiples of 9 in a * table
+*
+* Return: 0
+*
+*/
+void times_table(void)
+{
+int i, j;
 
+	for (i = 0; i <= 9; i++)
+	{
+	for (j = 0; j <= 9; j++)
+		{
+		printf("%d x %d = %d\n", i, j, i * j);
+		}
+	printf("\n");
+	}
+}
