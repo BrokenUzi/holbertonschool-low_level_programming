898edb9014235d69f6ee92bd683fbc83cc60779c
root <jcup143@gmail.com>
2023-05-30 19:31:12 -0700

    $commitMessage

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -1,2 +1,23 @@
 #include <stdio.h>
+#include "main.h"
+/**
+* print_last_digit - self xplain
+*
+* @n: last digit output (int)
+*
+* Return: 0
+*
+*/
+int print_last_digit(int n)
+{
+	int last_digit = n % 10;
 
+	if (last_digit < 0)
+	{
+	last_digit = -last_digit;
+	}
+
+	putchar(last_digit + '0');
+
+	return (last_digit);
+}
