15581ead186506b408c3426c5d562b3989676722
root <jcup143@gmail.com>
2023-05-30 21:08:32 -0700

    $commitMessage

--- functions_nested_loops/10-add.c
+++ functions_nested_loops/10-add.c
@@ -1,2 +1,20 @@
 #include <stdio.h>
+#include "main.h"
+/**
+* add - basic maths
+*
+* @a: int 1
+*
+* @b: int 2
+*
+* Return: 0
+*
+*/
+int add(int a, int b)
+{
+	int r;
 
+	r = a + b;
+
+	return (r);
+}
