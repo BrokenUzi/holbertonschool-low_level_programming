fde47382fafd86afc2acc7f7eb46d163dbb12434
root <jcup143@gmail.com>
2023-06-01 12:05:02 -0700

    $commitMessage

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
 #include "main.h"
 /**
+* print_numbers - prints #'s 0-9
 *
+* @i: is 0
 *
-*
-*
-*
+* Return: 0
 *
 */
 void print_numbers(void)
 {
 	int i;
+
 	for (i = 0; i <= 9;)
 	{
 	putchar('0' + i);
