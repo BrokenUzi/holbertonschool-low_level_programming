580c05ce538bcac04dd26af0c1fdfe7875678dcd
root <jcup143@gmail.com>
2023-06-02 20:12:37 -0700

    $commitMessage

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -9,15 +9,17 @@
 */
 void print_line(int n)
 {
-
 	if (n <= 0)
-		{
-		_putchar('\n');
+	{
+		_putchar('\n');  /* Print a newline and return if n is 0 or less */
 		return;
-		}
+	}
+
 	while (n > 0)
-		{
-		_putchar('_');
-		(n--);
-		}
+	{
+		_putchar('_');  /* Print the character '_' n times */
+		n--;
+	}
+
+	_putchar('\n');  /* Print a newline at the end of the line */
 }
