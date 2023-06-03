dfaedf6080a94ee1fb2291aff8c6ea01cf2d05a9
root <jcup143@gmail.com>
2023-06-02 21:35:36 -0700

    $commitMessage

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -13,15 +13,15 @@ void print_triangle(int size)
 
 	if (size <= 0)
 	{
-	_putchar('\n');
-	return;
+		_putchar('\n');
+		return;
 	}
 
 	for (i = 1; i <= size; i++)
 	{
-	for (j = 1; j <= i; j++)
-	_putchar('#');
+		for (j = 1; j <= i; j++)
+			_putchar('#');
 
-	_putchar('\n');
+		_putchar('\n');
 	}
 }
