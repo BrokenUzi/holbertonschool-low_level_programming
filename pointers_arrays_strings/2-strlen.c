91efa1187c5e97866998d115e9358ad232c84685
root <jcup143@gmail.com>
2023-06-04 17:13:33 -0700

    $commitMessage

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* _strlen - finds lenght of a string
+*
+* @s: Pointer to the string
+*
+* Return: Length of string
+*
+*/
+int _strlen(char *s)
+{
+	int length = 0;
+
+	while (*s)
+	{
+		length++;
+		s++;
+	}
+
+	return length;
+}
