e5ad06bace433106862346139e1f49e7453d0084
root <jcup143@gmail.com>
2023-06-04 17:02:45 -0700

    $commitMessage

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* reset_to_98 - Resets int value to 98
+*
+* @n: Point to int thats going 2 be reset
+*
+* Return: void
+*
+*/
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
