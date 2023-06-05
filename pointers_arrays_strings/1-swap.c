796ca5fa6d449261a481f64b3a62222172d54c07
root <jcup143@gmail.com>
2023-06-04 17:10:19 -0700

    $commitMessage

--- pointers_arrays_strings/1-swap.c
+++ pointers_arrays_strings/1-swap.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include "main.h"
 /**
+* swap_int - swaps the values of 2 int's
 *
+* @a: points to 1st int
 *
+* @b: points to 2nd int
 *
-*
-*
+* Return: void
 *
 */
 void swap_int(int *a, int *b)
