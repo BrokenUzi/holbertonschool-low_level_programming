a2bc9997123e775c2628a846e9b09a3486e3818e
root <jcup143@gmail.com>
2023-06-04 17:26:22 -0700

    $commitMessage

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* rev_string - reverses the string
+*
+* @s: Pointer to the string
+*
+*
+*
+*
+*/
+void rev_string(char *s)
+{
+	int length = 0;
+	int start = 0;
+	int end = 0;
+	char temp;
+
+	while (s[length] != '\0')
+		length++;
+
+	end = length - 1;
+
+	while (start < end)
+	{
+	temp = s[start];
+	s[start] = s[end];
+	s[end] = temp;
+
+	start++;
+	end--;
+	}
+}
+
