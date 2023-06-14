f3a652fbb6b3d3d322113f5fcdb3684c98864c57
root <jcup143@gmail.com>
2023-06-13 17:27:46 -0700

    $commitMessage

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -1,31 +1,32 @@
 #include <stdio.h>
 #include "main.h"
+
 /**
-* _strncat - Concatenates 2 strings up to n bytes
-*
-* @dest: Pointer to the dest string
-*
-* @src: Pointer to the src string
-*
-* @n: Max # of bytes to be copied from src
-*
-* Return: Pointer to the resulting string dest
-*/
+ * _strncat - Concatenates two strings
+ *
+ * @dest: The string to be appended
+ *
+ * @src: the string to be sent 2 dest.
+ *
+ * @n: # of bytes from src to be sent 2 dest.
+ *
+ * Return: pointer 2 result of string dest.
+ */
+
 char *_strncat(char *dest, char *src, int n)
+
 {
-	int dest_len = 0, i = 0;
+	int index = 0, dest_len = 0;
 
-	while (dest[dest_len] != '\0')
-	dest_len++;
+	while (dest[index++])
+		{
+		dest_len++;
+		}
 
-	while (src[i] != '\0' && i < n)
-	{
-	dest[dest_len] = src[i];
-	dest_len++;
-	i++;
-	}
-
-	dest[dest_len] = '\0';
-
+	for (index = 0; src[index] && index < n; index++)
+		{
+		dest[dest_len++] = src[index];
+		}
 	return (dest);
 }
+
