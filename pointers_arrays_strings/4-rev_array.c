2472dc737fe4521226ffb22fb7a15d3f17aa06a7
root <jcup143@gmail.com>
2023-06-13 17:39:15 -0700

    $commitMessage

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* reverse_array - reverses array of length n
+*
+* @a: obj. array
+*
+* @n: length of given array
+*
+* Return: void
+*/
+
+void reverse_array(int *a, int n)
+{
+	int x = 0;
+	int y;
+	int z;
+
+	while (x < n && a[n] != '\0')
+		{
+		x++;
+
+		x--;
+		}
+
+	for (y = 0; y < x; y++)
+		{
+
+		z = a[x];
+		a[x] = a[y];
+		a[y] = z;
+
+		x--;
+		}
+}
