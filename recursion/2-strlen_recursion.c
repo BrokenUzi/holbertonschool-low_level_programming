e1dfe1c114065392ecd9ac5eeef7f489a675e7d7
root <jcup143@gmail.com>
2023-06-14 14:06:51 -0700

    $commitMessage

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -1,2 +1,23 @@
 #include <stdio.h>
+#include "main.h"
 
+/**
+*
+*
+*
+*
+*
+*
+*
+*/
+
+int _strlen_recursion(char *s)
+{
+
+	if (*s != '\0')
+	{
+		return (1 + _strlen_recursion(s + 1));
+	}
+	else
+	return (0);
+}
