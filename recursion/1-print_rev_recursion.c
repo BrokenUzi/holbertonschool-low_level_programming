2c38b2f055e855601f99c7507b0299b40e061da3
root <jcup143@gmail.com>
2023-06-14 13:21:08 -0700

    $commitMessage

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -1,2 +1,23 @@
 #include <stdio.h>
+#include "main.h"
 
+/**
+* _print_rev_recursion - prints a string in reverse
+*
+* @s - string
+*
+* Return: void
+*/
+
+void _print_rev_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_print_rev_recursion(s - 1);
+		_putchar(*s);
+	}
+	else
+	{
+		_puthcar('\n');
+	}
+}
