dd4384016f26100362dee1270f8396888c7474cd
root <jcup143@gmail.com>
2023-06-13 20:14:23 -0700

    $commitMessage

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* _strncpy - copy string from src to dest to n bytes
+*
+* @dest: self xplain
+*
+* @src: self xplain
+*
+* @n: # of bytes of src 2 b copied
+*
+* Return: dest
+*/
+char *_strncpy(char *dest, char *src, int n)
+{
+	int x;
+
+	for (x = 0; x < n && src[x] != '\0'; x++)
+	dest[x] = src[x];
+
+	while (x < n)
+		{
+		dest[x] = '\0';
+		x++;
+		}
+
+	return (dest);
+}
