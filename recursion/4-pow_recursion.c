eb6a4cab9b706aaa45f83df25707d28ee95ed990
root <jcup143@gmail.com>
2023-06-15 12:25:02 -0700

    $commitMessage

--- recursion/4-pow_recursion.c
+++ recursion/4-pow_recursion.c
@@ -1,2 +1,29 @@
 #include <stdio.h>
+#include "main.h"
 
+/**
+* _pow_recursion - returns val x raised 2 the y power
+*
+* @x:
+*
+* @y: 
+*
+* Return: 
+*/
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	if (y == 0)
+	{
+		return (1);
+	}
+	if (y > 0)
+	{
+		return (x * _pow_recursion(x, (y - 1)));
+	}
+return (0);
+}
