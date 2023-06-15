365d0a5d9f8a8b40b5397beb03ad4a60b0bc9763
root <jcup143@gmail.com>
2023-06-15 12:27:22 -0700

    $commitMessage

--- recursion/4-pow_recursion.c
+++ recursion/4-pow_recursion.c
@@ -4,11 +4,11 @@
 /**
 * _pow_recursion - returns val x raised 2 the y power
 *
-* @x:
+* @x: Obj. variable
 *
-* @y: 
+* @y: Raised to the power of Y
 *
-* Return: 
+* Return: 0
 */
 
 int _pow_recursion(int x, int y)
