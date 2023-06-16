ac4bda2beef91ddbdccbee0b7f1f7437d0fa707e
root <jcup143@gmail.com>
2023-06-15 23:45:39 -0700

    $commitMessage

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -1,2 +1,18 @@
 #include <stdio.h>
+#include "main.h"
 
+/**
+* is_prime_number - finds out if obj num is prime
+*
+* @n: obj value
+*
+* Return: 1 if prime. 0 if not prime
+*/
+
+int is_prime_number(int n)
+{
+	if (n % 2 != 0 || n % 3 != 0 || n >= 1)
+		return (1);
+	else
+		return (0);
+}
