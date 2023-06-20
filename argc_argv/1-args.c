84c0e817da1c309eb94fddea3808364a1e753da2
root <jcup143@gmail.com>
2023-06-19 19:08:39 -0700

    $commitMessage

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -1,2 +1,20 @@
 #include <stdio.h>
 
+/**
+* main - prints how many arguemnts are passed thru the command line
+*
+* @argc: number of command line arguments
+*
+* @argv: 
+*
+* Return: 0
+*/
+
+int main(int argc, char *argv[])
+{
+	if (argv)
+	{
+		printf("%d\n", argc - 1);
+	}
+return (0);
+}
