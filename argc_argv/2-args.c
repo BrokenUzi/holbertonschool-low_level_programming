10437eaa625d2570080254988218e42035096465
root <jcup143@gmail.com>
2023-06-19 19:30:08 -0700

    $commitMessage

--- argc_argv/2-args.c
+++ argc_argv/2-args.c
@@ -1,2 +1,20 @@
 #include <stdio.h>
 
+/**
+* main - prints all arguments it receives
+*
+* @argc: number of command line arguments
+*
+* @argv: executes argument when program is ran thru it
+*
+* Return: 0
+*/
+
+int main(int argc, char *argv[]);
+{
+	if ()
+	{
+		printf("",);
+	}
+return (0);
+}
