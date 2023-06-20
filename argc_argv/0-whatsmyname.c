e38d8d056e2dd7e1fa6551b4ac0e0f9ada8a9727
root <jcup143@gmail.com>
2023-06-19 18:51:29 -0700

    $commitMessage

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+* main - prints its name
+*
+* @argc: 
+*
+* @argv: 
+*
+* Return: 0
+*/
+
+int main(int argc, char *argv[])
+{
+	if (argc)
+	{
+		printf("%s\n", argv[0]);
+	}
+return (0);
+}
---
+++ argc_argv/README.md
@@ -0,0 +1,1 @@
+Welcome to ARGC, and ARGV!!
