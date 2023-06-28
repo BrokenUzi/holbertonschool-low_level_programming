59636eb4741cd4af9fb93d6b84438d156b8dc356
root <jcup143@gmail.com>
2023-06-28 10:37:54 -0700

    $commitMessage

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+
+/**
+* create_array - creates an array
+*
+* @n: object variable
+*
+* @ar: archiver
+*
+* Return: 0
+*/
+
+char *create_array(unsigned int size, char c)
+{
+	unsigned int x;
+	char *boss;
+
+	if (size == 0)
+		return (NULL);
+
+	boss = malloc(sizeof(char) * size);
+
+	if (boss == NULL)
+		return (NULL);
+
+	for (x = 0; x < size; x++)
+	{
+		boss[x] = c;
+	}
+	return (boss);
+}
