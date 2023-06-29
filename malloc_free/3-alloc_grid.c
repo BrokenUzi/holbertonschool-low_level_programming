19af707470eef2b62fc1a335330ad7348a775160
root <jcup143@gmail.com>
2023-06-29 14:52:00 -0700

    $commitMessage

---
+++ malloc_free/3-alloc_grid.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+*
+*
+*
+*
+*
+*
+*/
+
+int **alloc_grid(int width, int height)
+{
+	int **uzi = 0;
+	int x = 0;
+	int y = 0;
+
+	uzi = malloc(sizeof(int *) * h);
+
+	if ((w * h) < 1 || uzi == NULL)
+		return (NULL);
+
+	for (; y < h; y++)
+	{
+		uzi[y] = malloc(sizeof(int) * w);
+
+		if (uzi[y] == NULL)
+		{
+			for (; y >= 0; y--)
+				free(uzi[y]);
+
+			free(uzi);
+
+			return (NULL);
+		}
+	}
+
+	for (; x < w; x++)
+		for (; y < h; y++)
+			uzi[x][y] = 0;
+
+	return (uzi);
+}
