e31c1a2c470968e2a154aedf560b0a352d427f09
root <jcup143@gmail.com>
2023-06-29 14:27:03 -0700

    $commitMessage

---
+++ malloc_free/2-str_concat.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include "main.h"
+#include <string.h>
+#include <stdlib.h>
+
+/**
+*
+*
+*
+*
+*
+*
+*
+*/
+
+char *str_concat(char *s1, char *s2)
+{
+	char *dest;
+	int a;
+	int b;
+	int len1, len2;
+
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+
+	len1 = strlen(s1);
+	len2 = strlen(s2) + 1;
+
+	dest = malloc(sizeof(char) * (len1 + len2));
+
+	if (dest == 0)
+	{
+		return (NULL);
+	}
+
+	for (a = 0; a < len1; a++)
+	{
+		dest[a] = s1[a];
+	}
+	for (b = 0; b < len2; b++)
+	{
+		a++;
+		dest[b] = s2[b];
+	}
+return (dest);
+}
