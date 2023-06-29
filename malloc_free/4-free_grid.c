226102972fdc766dbc92cbebabc6a1ffd66595e2
root <jcup143@gmail.com>
2023-06-29 15:20:10 -0700

    $commitMessage

---
+++ malloc_free/4-free_grid.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+
+/**
+* free_grid - frees a 2 dimensional grid
+*
+* @grid: address of the two dimensional grid
+*
+* @width: height of the grid
+*
+* Return: Nothing
+*/
+
+void free_grid(int **grid, int height)
+{
+	int y = height;
+
+	if (grid != NULL)
+		for (; y >= 0; y--)
+			free(grid[y]);
+
+	free(grid);
+}
