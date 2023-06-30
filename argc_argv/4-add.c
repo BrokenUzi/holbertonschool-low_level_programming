50099e336040d8a54ecdf61efaebceb1a74cf441
root <jcup143@gmail.com>
2023-06-30 14:45:50 -0700

    $commitMessage

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -1,2 +1,48 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
 
+/**
+*
+*
+*
+*
+*
+*/
+
+int main(int argc, char *argv[])
+{
+	int a = 1;
+	int b = 0;
+	char *c;
+
+	if (argc > 1)
+	{
+		for (; a < argc; a++)
+		{
+			c = argv[a];
+
+			while (*c)
+			{
+				if (!isdigit(*c))
+				{
+
+					printf("Error\n");
+					return (1);
+				}
+
+				c++;
+
+			}
+
+			b += atoi(argv[a]);
+		}
+
+		printf("%d\n", b);
+	}
+
+	else
+		printf("0\n");
+
+	return (0);
+}
