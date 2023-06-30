41a975f8f784747adcebbb3350765b8f2f862677
root <jcup143@gmail.com>
2023-06-30 13:57:58 -0700

    $commitMessage

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -1,2 +1,23 @@
 #include <stdio.h>
+#include <stdlib.h>
 
+/**
+* main - mult 2 args in argv can only do 2 otherwise error
+*
+* @argc: argument count
+*
+* @argv: argument vector
+*
+* Return: 0
+*/
+
+int main(int argc, char *argv[])
+{
+	if ((argc - 1) == 2)
+		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+
+	else
+		printf("Error\n");
+
+	return (0);
+}
