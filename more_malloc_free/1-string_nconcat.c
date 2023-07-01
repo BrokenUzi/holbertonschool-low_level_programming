5b71174a03a72799eac19f05ddb03f196dbf5c2c
root <jcup143@gmail.com>
2023-06-30 19:01:53 -0700

    $commitMessage

---
+++ more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+#include <string.h>
+
+/**
+*
+*
+*
+*
+*
+*
+*
+*/
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *dest;
+	int a;
+	int b;
+	int len1, len2;
+
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+
+	len1 = strlen(s1);
+	len2 = (n + 1);
+
+	dest = malloc(sizeof(char) * (len1 + len2));
+
+	if (dest == 0)
+	{
+		return (NULL);
+	}
+
+	for (a = 0; a < len1; a++)
+	{
+		dest[a] = s1[a];
+	}
+	for (b = 0; b < len2; b++, a++)
+	{
+		dest[a] = s2[b];
+	}
+dest[a] = '\0';
+return (dest);
+}
