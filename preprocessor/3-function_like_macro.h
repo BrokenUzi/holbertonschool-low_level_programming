7286f03a1400a21902983eaaf0ce6e4208ea282b
root <jcup143@gmail.com>
2023-06-30 21:56:29 -0700

    $commitMessage

--- preprocessor/3-function_like_macro.h
+++ preprocessor/3-function_like_macro.h
@@ -1,8 +1,8 @@
-#ifndef 3_function_like_macro_h
-#define 3_function_like_macro_h
+#ifndef _3_function_like_macro_h_
+#define _3_function_like_macro_h_
 
 #ifndef ABS
-#define ABS(x) ((x) < 0)
+#define ABS(x) ((x) < 0 ? ((x) * -1) : (x))
 #endif
 
 #endif
