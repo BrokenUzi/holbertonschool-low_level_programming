4b78e2a328eb88dbcc8b14f3dd2991b5a2591938
root <jcup143@gmail.com>
2023-07-03 14:37:20 -0700

    $commitMessage

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,2 +1,28 @@
-#ifndef
+#ifndef _DOG_H_
+#define _DOG_H_
 
+/**
+* struct dog - struct for the dog
+* @name: name of dog as a string
+* @age: dogs age as a float
+* @owner: owner of the dog as a string
+*/
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+/**
+*typedef dog_t - defines a new name for struct dog
+*/
+
+typedef struct dog dog_t;
+int _putchar(char c);
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+#endif
