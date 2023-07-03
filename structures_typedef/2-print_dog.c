f571c1bb3595f7b3f911c0508e5d7624560a5716
root <jcup143@gmail.com>
2023-07-03 14:49:17 -0700

    $commitMessage

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+*
+*
+*
+*/
+void print_dog(struct dog *d)
+{
+	if (d != NULL)
+	{
+		printf("Name: %s\n", (d->name) ? d->name : "(nill)");
+		printf("Age: %f\n", (d->age) ? d->age : 0);
+		printf("Owner: %s\n", (d->owner) ? d->owner : "(nill)");
+	}
+}
