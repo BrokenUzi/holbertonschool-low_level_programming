c4de8c1fd093ace454ed94172c94dd9defdd0cf2
root <jcup143@gmail.com>
2023-07-03 18:07:49 -0700

    $commitMessage

---
+++ function_pointers/3-get_op_func.c
@@ -0,0 +1,29 @@
+#include "3-calc.h"
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* get_op_func - supplies arithmetic func if input matches ops variant
+* @s: input 'sign' to be checked against variants in ops
+* Return: function corr. to sign, if found - NULL otherwise
+*/
+
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i = 0;
+
+	if (s != NULL)
+		while (ops[i].op != NULL && *(ops[i].op) != *s)
+			i++;
+
+	return (ops[i].f);
+}
---
+++ function_pointers/3-main.c
@@ -0,0 +1,38 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stddef.h>
+#include <stdlib.h>
+
+/**
+* main - print calc of 2 args based on & separated by arithmetic operator
+* @argc: argument count, must be 4
+* @argv: argument vector - el. 1 & 3 must be nos. & 2, valid operator ('sign')
+* Return: 0 upon success, exit codes with error upon unacceptable input
+*/
+
+int main(int argc, char **argv)
+{
+	char *sign = argv[2];
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	if (get_op_func(sign) == NULL || sign[1])
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	if ((*sign == '/' || *sign == '%') && atoi(argv[3]) == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	printf("%d\n", get_op_func(sign)(atoi(argv[1]), atoi(argv[3])));
+
+	return (0);
+}
