cd83236eadd490f5749bfc446f05f60d9af1b959
root <jcup143@gmail.com>
2023-07-04 14:02:45 -0700

    $commitMessage

---
+++ variadic_functions/1-print_numbers.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - prints numbers delimited by input separator
+ * @separator: delimiter between numbers to be printed
+ * @n: number of arguments following itself
+ * Return: void
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list set;
+	unsigned int x = 0;
+	int ro = 0;
+
+	va_start(set, n);
+
+	for (; x < n; x++)
+	{
+		ro = va_arg(set, unsigned int);
+		printf("%d", ro);
+
+		if (separator != NULL && x != n - 1)
+			printf("%s", separator);
+	}
+
+	va_end(set);
+
+	printf("\n");
+}
---
+++ variadic_functions/3-print_all.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+ * print_all - prints inputs of certain data types
+ * @frmt: list of data types of input arguments
+ * Return: void
+ */
+
+void print_all(const char * const frmt, ...)
+{
+	va_list surf;
+	char *dude, *delim = ", ";
+	int x = 0;
+
+	va_start(surf, frmt);
+	while (frmt && frmt[x])
+	{
+		switch (frmt[x])
+		{
+		case 'c':
+			printf("%c%s", (char) va_arg(surf, int), delim);
+			break;
+		case 'i':
+			printf("%d%s", va_arg(surf, int), delim);
+			break;
+		case 'f':
+			printf("%f%s", va_arg(surf, double), delim);
+			break;
+		case 's':
+			dude = va_arg(surf, char *);
+			if (!dude)
+				dude = "(nil)";
+			printf("%s%s", dude, delim);
+			break;
+		}
+		if (!frmt[x + 2])
+			delim = "";
+		x++;
+	}
+	printf("\n");
+	va_end(surf);
+}
