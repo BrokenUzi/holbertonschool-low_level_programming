ee0943a456fa97f077e6fbb9e4e02144e18c26e6
root <jcup143@gmail.com>
2023-07-13 15:41:22 -0700

    $commitMessage

---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+
+/**
+* list_len - returns the # of elements in a linked list
+*
+*
+*
+*/
+
+size_t list_len(const list_t *h)
+{
+	int x = 0;
+	char *s1 = NULL;
+
+	if (h == NULL)
+	{
+		return (0);
+	}
+	for (; h; x++)
+	{
+		s1 = h->str;
+		if (s1 != NULL)
+		{
+			printf("[%d] %s\n", h->len, h->str);
+		}
+		h = h->next;
+	}
+	return (x);
+}
