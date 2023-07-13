56a7a8f5569ab2f57e35389e1036ee5246a89cd8
root <jcup143@gmail.com>
2023-07-13 13:43:33 -0700

    $commitMessage

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -1,17 +1,20 @@
-#include <stdio.h>
+#include "lists.h"
 
 /**
+* size_t - prints all elements of list_t list
 *
+* @h: node head
 *
-*
-*
-*
-*
+* Return: all elements of list_t
 */
 
 size_t print_list(const list_t *h)
 {
-	if (str == NULL)
+	int x = 0;
+	char *s1 = NULL;
+
+	if (h == NULL)
 	{
 		printf("[0] (nill)");
 	}
+return (0);
--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -1,4 +1,9 @@
-#include <std.lib>
+#ifndef _LISTS_H_
+#define _LISTS_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 /**
  * struct list_s - singly linked list
@@ -20,3 +25,5 @@ size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
+
+#endif
