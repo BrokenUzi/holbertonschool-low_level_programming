cf7388780db077aad1b7ebc0e29794a8abfaf58f
root <jcup143@gmail.com>
2023-07-15 22:16:20 -0700

    $commitMessage

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -1,15 +1,28 @@
 #include "lists.h"
+#include <stdlib.h>
+#include <string.h>
 
 /**
-*
-*
-*
+* list_t - 
+* @head: head of node
+* @str: string
+* Return: Head
 */
 
 list_t *add_node(list_t **head, const char *str)
 {
-	struct node* newNode = (struct node*) malloc(sizeof(struct node));
-	newNode->data  = str;
-	newNode->next = head;
-	head = newNode;
+	list_t *new_node;
+
+new_node = malloc(sizeof(list_t));
+
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
+new_node->str = strdup(str);
+new_node->len = strlen(str);
+new_node->next = *head;
+*head = new_node;
+
+return (new_node);
 }
