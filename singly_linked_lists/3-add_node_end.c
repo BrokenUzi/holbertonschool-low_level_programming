097eef4bbd4d4d71d92096c954bf059207fb7a64
root <jcup143@gmail.com>
2023-07-17 15:44:10 -0700

    $commitMessage

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -1,35 +1,32 @@
 #include "lists.h"
-#include <stdlib.h>
-#include <string.h>
 
 /**
-* add_node_end - add's a new node at the end
-* @head: head of node
-* @str: string
-* Return: Head
-*/
+ * add_node_end - create node & add it at the end of our linked list
+ * @head: pointer to head of linked list
+ * @s1: name of the new node
+ * Return: address of new node, NULL if mem alloc fails
+ */
 
 list_t *add_node_end(list_t **head, const char *str)
 {
+	int x = 0;
 	list_t *end_node;
 	list_t *position;
 
-end_node = malloc(sizeof(list_t));
+	end_node = malloc(sizeof(list_t));
 
 	if (end_node == NULL)
-	{
 		return (NULL);
-	}
 
-end_node->str = strdup(str);
-end_node->len = strlen(str);
-end_node->next = NULL;
+	for (; str[x]; x++)
+		;
 
-	if (head == NULL)
-	{
+	end_node->str = strdup(str);
+	end_node->len = x;
+	end_node->next = NULL;
+
+	if (*head == NULL)
 		*head = end_node;
-	}
-
 	else
 	{
 		position = *head;
@@ -40,5 +37,5 @@ end_node->next = NULL;
 		position->next = end_node;
 	}
 
-return (end_node);
+	return (end_node);
 }
