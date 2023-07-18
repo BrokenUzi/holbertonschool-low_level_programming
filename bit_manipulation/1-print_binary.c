8bce826d89bf4328c4403c0c2b838aa8790520fd
root <jcup143@gmail.com>
2023-07-17 19:56:34 -0700

    $commitMessage

---
+++ bit_manipulation/1-print_binary.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* print_binary - prints binary equivalent of base10 input
+* @input: object number of base10
+* Return: void
+*/
+
+void print_binary(unsigned long int input)
+{
+	if (input > 1)
+		print_binary(input >> 1);
+
+	_putchar((input & 1) + 48);
+}
