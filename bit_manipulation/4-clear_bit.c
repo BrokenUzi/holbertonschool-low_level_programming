2b598ac33b2b5d825ac90bf57a6e43b18a5ed7eb
root <jcup143@gmail.com>
2023-07-17 20:27:08 -0700

    $commitMessage

---
+++ bit_manipulation/4-clear_bit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* clear_bit - sets bit at index of input to 0
+* @input: object number
+* @index: position within binary of input
+* Return: 1 if success, -1 if error
+*/
+
+int clear_bit(unsigned long int *input, unsigned int index)
+{
+	if (index > 63)
+		return (-1);
+
+	*input &= ~(1 << index);
+
+	return (1);
+}
