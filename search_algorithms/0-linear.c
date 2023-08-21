58896c4e057038af2276395c7375c387abe51ad0
root <jcup143@gmail.com>
2023-08-21 16:00:43 -0700

    added all the main files and Linear search.

---
+++ search_algorithms/0-linear.c
@@ -0,0 +1,26 @@
+#include "search_algos.h"
+
+/**
+* linear_search - searches through the array for value using linear search algo.
+* @array: array
+* @size: size of array
+* @value: value that is being looked for
+* Return: index where and if value found, -1 upon value not found or NULL array
+*/
+
+int linear_search(int *array, size_t size, int value)
+{
+	size_t x = 0;
+
+	if (!array || !size)
+		return (-1);
+
+	for (; x < size; x++)
+	{
+		printf("Value checked array[%ld] = [%d]\n", x, array[i]);
+		if (array[x] == value)
+			return (x);
+	}
+
+	return (-1);
+}
